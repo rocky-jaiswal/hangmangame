27a7e6b7d970bccfe6b8b2e7f714bfb177b427e0
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2018-03-31 21:48:51 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hangmangame "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "classic hangman game"
+  :url "https://github.com/rocky-jaiswal/hangmangame"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
